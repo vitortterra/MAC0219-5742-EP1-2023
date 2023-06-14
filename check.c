c77f04adf98d5921183aa1fc1398177d1a0f33b4
Vitor Tessari Terra <vitortterra@users.noreply.github.com>
2023-06-14 20:47:30 +0000

    check things in check

--- check.c
+++ check.c
@@ -1,15 +1,18 @@
 #include "lga_base.h"
 #include "lga_seq.h"
+#include "lga_omp.h"
+#include "lga_pth.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void parse_arguments(int argc, char* argv[], int* grid_size_ptr) {
     int ret;
 
     if (argc != 3){
-        printf("usage: ./lca --grid_size <grid_size>\n");
+        printf("usage: ./check --grid_size <grid_size>\n");
         exit(EXIT_FAILURE);
     }
 
@@ -30,18 +33,41 @@ void parse_arguments(int argc, char* argv[], int* grid_size_ptr) {
     }
 }
 
+bool are_grids_equal(byte* grid_a, byte* grid_b, int grid_size) {
+    for (int idx = 0; idx < grid_size*grid_size; idx++)
+        if (grid_a[idx] != grid_b[idx])
+            return false;
+    
+    return true;
+}
+
 int main(int argc, char* argv[]) {
+    byte* grid_ref;
     byte* grid_1;
     byte* grid_2;
     int grid_size;
 
     parse_arguments(argc, argv, &grid_size);
 
+    grid_ref = allocate_grid(grid_size);
     grid_1 = allocate_grid(grid_size);
     grid_2 = allocate_grid(grid_size);
 
-    simulate_seq(grid_1, grid_2, grid_size);
+    simulate_seq(grid_ref, grid_2, grid_size);
 
+    simulate_omp(grid_1, grid_2, grid_size);
+    if (are_grids_equal(grid_ref, grid_1, grid_size))
+        printf("Resultado OpenMP igual ao sequencial\n");
+    else
+        printf("Resultado OpenMP DIFERENTE do sequencial\n");
+    
+    simulate_pth(grid_1, grid_2, grid_size);
+    if (are_grids_equal(grid_ref, grid_1, grid_size))
+        printf("Resultado Pthreads igual ao sequencial\n");
+    else
+        printf("Resultado Pthreads DIFERENTE do sequencial\n");
+
+    free(grid_ref);
     free(grid_1);
     free(grid_2);
 
