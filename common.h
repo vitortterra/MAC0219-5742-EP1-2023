c78600566bb448f183d13fc01f3b4c94ca50f21e
Vitor Tessari Terra <vitortterra@users.noreply.github.com>
2023-06-14 05:10:57 +0000

    just to make sure

--- common.h
+++ common.h
@@ -3,6 +3,6 @@
 #define MIN_GRID_SIZE 8
 #define MAX_GRID_SIZE 4096
 #define NUM_DIRECTIONS 6
-#define ind2d(i,j) (i)*(grid_size)+j
+#define ind2d(i,j) ((i)*(grid_size)+(j))
 
 typedef unsigned char byte;
