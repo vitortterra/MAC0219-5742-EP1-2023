efaa5a7a07851b6eb4d9b74a4ae25ab86624fbae
Vitor Tessari Terra <vitortterra@users.noreply.github.com>
2023-06-25 06:41:00 +0000

    refactor take 1

--- src/lga_seq.c
+++ src/lga_seq.c
@@ -1,47 +1,55 @@
 #include "lga_base.h"
 #include "lga_seq.h"
 
-// Atualiza o LGA a partir do estado em grid_in,
-// escrevendo o resultado em grid_out
-static void update(byte *grid_in, byte *grid_out, int grid_size) {
-    // Etapa de propagacao: atualiza posicoes
-    // e verifica colisoes com paredes
-    for (int i = 0; i < grid_size; i++) {
-        for (int j = 0; j < grid_size; j++) {
-            // Para cada celula, para cada direcao, verifica se
-            // ha particula indo naquela direcao
-            for (int dir = 0; dir < NUM_DIRECTIONS; dir++) {
-                byte dir_mask = 0x01 << dir;
+// Encontra o proximo estado da celula em (i, j): grid_out[ind2d(i,j)],
+// a partir do estado atual (grid_in)
+byte get_next_cell(int i, int j, byte *grid_in, byte *grid_out, int grid_size) {
+    byte next_cell = EMPTY;
 
-                // Caso haja uma particula indo na direcao dir,
-                // atualiza o vizinho colocando nele uma particula
-                // na mesma direcao
-                if (grid_in[ind2d(i,j)] & dir_mask) {
-                    int di = directions[i%2][dir][0];
-                    int dj = directions[i%2][dir][1];
-                    int n_i = i + di;
-                    int n_j = j + dj;
+    // Etapa de propagacao:
+    // Para cada direcao, verifica se ha particula vindo daquela direcao
+    for (int dir = 0; dir < NUM_DIRECTIONS; dir++) {
 
-                    if (inbounds(n_i, n_j, grid_size)) {
-                        if (grid_in[ind2d(n_i,n_j)] == WALL)
-                            wall_collision(i, j, grid_out, grid_size, dir);
-                        
-                        // Se estiver dentro dos limites e nao colidir com parede, propaga
-                        else
-                            grid_out[ind2d(n_i,n_j)] |= dir_mask;
+        // rev_dir: direcao oposta a dir
+        int rev_dir = (dir + NUM_DIRECTIONS/2) % NUM_DIRECTIONS;
+        byte rev_dir_mask = 0x01 << rev_dir;
 
-                        // Remove a particula da celula anterior
-                        grid_in[ind2d(i,j)] &= ~dir_mask;
-                    }
-                }
+        // (n_i, n_j): coordenadas do vizinho da celula (i, j) na direcao dir
+        int di = directions[i%2][dir][0];
+        int dj = directions[i%2][dir][1];
+        int n_i = i + di;
+        int n_j = j + dj;
+
+        // Verifica se o vizinho esta dentro do grid
+        if (inbounds(n_i, n_j, grid_size)) {
+
+            // Se o vizinho for parede, eh possivel que apareca
+            // em (i, j) uma particula que colidgit addiu com a parede
+            if (grid_in[ind2d(n_i,n_j)] == WALL) {
+                // TODO: handle wall collision
+            } 
+            // Caso haja uma particula vindo do vizinho para a celula,
+            // atualiza a celula colocando nela essa particula
+            else if (grid_in[ind2d(n_i, n_j)] & rev_dir_mask) {
+                next_cell |= rev_dir_mask;
             }
         }
     }
 
-    // Etapa de colisao: verifica colisoes entre particulas
+    // Etapa de colisao: verifica se apos a propagacao, 
+    // houve colisao entre particulas em (i, j)
+    return particles_collision(next_cell);
+}
+
+// Atualiza o LGA a partir do estado em grid_in,
+// escrevendo o resultado em grid_out
+static void update(byte *grid_in, byte *grid_out, int grid_size) {
     for (int i = 0; i < grid_size; i++) {
         for (int j = 0; j < grid_size; j++) {
-            grid_out[ind2d(i,j)] = particles_collision(grid_out[ind2d(i,j)]);
+            if (grid_in[ind2d(i,j)] == WALL) 
+                grid_out[ind2d(i,j)] = WALL;
+            else 
+                grid_out[ind2d(i,j)] = get_next_cell(i, j, grid_in, grid_out, grid_size);
         }
     }
 }
